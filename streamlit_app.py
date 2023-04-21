675d2c6a037cff1f88c87ab2e983b1ec5fd8c6b1
malodevinod111 <131424853+malodevinod111@users.noreply.github.com>
2023-04-21 10:24:03 +0530

    Update streamlit_app.py
    added mutiselect

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,7 @@ import streamlit
 import pandas
 streamlit.title("My Parent New Helthy Diner")
 streamlit.header('🍞 Breakfast Menu')
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.multiselect("Pick some fruits: ",my_fruit_list.index)
 streamlit.dataframe(my_fruit_list)
