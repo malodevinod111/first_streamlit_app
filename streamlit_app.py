0963aa898eb7912bbe5f55f29ba1999e08d33ed4
malodevinod111 <131424853+malodevinod111@users.noreply.github.com>
2023-04-21 10:25:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ streamlit.title("My Parent New Helthy Diner")
 streamlit.header('🍞 Breakfast Menu')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.multiselect("Pick some fruits: ",my_fruit_list.index)
+streamlit.multiselect("Pick some fruits: ",my_fruit_list.fruit)
 streamlit.dataframe(my_fruit_list)
